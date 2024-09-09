a2dce3d19ecf220f78c394177f0e4737028e4ffd
Ben <feba23@live.com>
2024-09-09 13:10:07 -0500

    Base functionality for separate audio thread and audio being read in realtime by another thread

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod audio_modifiers;
 mod realtime_playback;
 
 use crate::realtime_playback::playback;
-use log::info;
+use std::io::{self};
 use std::sync::{Arc, LazyLock, Mutex};
 use std::thread;
 use std::time::Duration;
@@ -10,9 +10,8 @@ use std::time::Duration;
 static AUDIO_DATA: LazyLock<Arc<Mutex<Vec<f32>>>> =
     LazyLock::new(|| Arc::new(Mutex::new(Vec::new())));
 
-fn main() {
+fn main() -> Result<(), io::Error> {
     env_logger::init();
-
     let _ = thread::spawn(move || {
         let _ = playback(Arc::clone(&AUDIO_DATA));
     });
@@ -20,11 +19,6 @@ fn main() {
     loop {
         thread::sleep(Duration::from_millis(1));
         let audio = AUDIO_DATA.lock().unwrap().clone();
-        match audio.get(600) {
-            Some(value) => {
-                info!("{}", value);
-            }
-            None => {}
-        }
+        println!("{}", audio.get(600).unwrap_or(&0.0f32));
     }
 }
